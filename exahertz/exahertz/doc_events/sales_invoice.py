ea4a790c53b4b07569a93d3da46d8561697dc712
bibinvabaraham <bibincv237@gmail.com>
2023-11-23 16:10:12 +0530

    feat:link meter data

--- exahertz/exahertz/doc_events/sales_invoice.py
+++ exahertz/exahertz/doc_events/sales_invoice.py
@@ -11,3 +11,18 @@ def get_print_er(from_cur, to_cur):
 def get_print_tw(amount, curr):
     return money_in_words(amount, curr)
     
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_meter_list(doctype, txt, searchfield, start, page_len, filters):
+    query = ""
+    if txt:
+        query += "AND meter_id LIKE '%{txt}%'".format(txt=txt)
+    data = frappe.db.sql("""
+        SELECT meter_id
+        FROM `tabMeter Details` md
+        INNER JOIN `tabMeter P69` mp
+        ON md.parent = mp.name
+        WHERE mp.customer_id = "{customer}"
+        {query}
+    """.format(customer=filters.get("customer"), query=query))
+    return data
--- exahertz/public/js/sales_invoice.js
+++ exahertz/public/js/sales_invoice.js
@@ -2,7 +2,8 @@ frappe.ui.form.on('Sales Invoice', {
     onload:function(frm){
         frappe.xcall("exahertz.exahertz.doc_events.sales_invoice.get_print_er", {"from_cur": frm.doc.currency, "to_cur": "USD"}).then(data=>{
             frm.set_value("custom_print_exchange_rate", data)
-        })
+        });
+        frm.trigger('get_meter_list');
     },
     custom_print_currency: function(frm){
         frappe.xcall("exahertz.exahertz.doc_events.sales_invoice.get_print_er", {"from_cur": frm.doc.currency, "to_cur": frm.doc.custom_print_currency}).then(data=>{
@@ -14,6 +15,21 @@ frappe.ui.form.on('Sales Invoice', {
         frm.set_value("custom_print_amount_usd", total * frm.doc.custom_print_exchange_rate)
         frappe.xcall("exahertz.exahertz.doc_events.sales_invoice.get_print_tw", {"amount": total*frm.doc.custom_print_exchange_rate, curr: frm.doc.custom_print_currency}).then(data=>{
             frm.set_value("custom_print_in_words", data)
+        })
+    },
+    customer:function(frm){
+        frm.trigger('get_meter_list');
+    },
+    get_meter_list:function(frm){
+        frm.set_query('custom_meter_p69', function(){
+            if(frm.doc.customer){
+                return {
+                    query: "exahertz.exahertz.doc_events.sales_invoice.get_meter_list",
+                    filters: {
+                        customer: frm.doc.customer
+                    }
+                }
+            }
         })
     }
 })
